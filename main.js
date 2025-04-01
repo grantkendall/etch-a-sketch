29ddf100ea9acd1cb17a8ef68919604f7f09e3f9
grantkendall <137111847+grantkendall@users.noreply.github.com>
2025-04-01 11:07:23 -0500

    Add color picker

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const container = document.querySelector(".grid-container");
 const resetBtn = document.querySelector(".reset-btn");
 const blueBtn = document.querySelector(".blue-btn");
 const rainbowBtn = document.querySelector(".rainbow-btn");
+const colorPicker = document.querySelector(".color-picker");
 
 const grayscale = [
     "#000000",
@@ -9,7 +10,7 @@ const grayscale = [
     "#444444",
     "#666666",
     "#888888",
-]
+];
 
 const rainbow = [
     "#88A5BF", // Blue-grey
@@ -17,25 +18,30 @@ const rainbow = [
     "#88A61B", // Green
     "#F28C0F", // Orange
     "#F2620F", // Red-orange
-]
+];
 
 let palette = "black"; 
 
 document.addEventListener("mouseover", squareListener);
+
 resetBtn.addEventListener("click", () => reconstructGrid());
+
 blueBtn.addEventListener("click", () => changePalette("blue"));
 rainbowBtn.addEventListener("click", () => changePalette("rainbow"));
+colorPicker.addEventListener("input", () => changePalette(colorPicker.value));
 
 function changePalette(color) {
     palette = color;
-}
+};
 
-function getUserInput() {
-    input = prompt("How many rows and columns?");
-    if (input > 100) {
-        input = 100;
+function squareListener(event) {
+    let t = event.target;
+    if(t.className == "square") {
+        t.style.backgroundColor = palette;
+        if (palette == "rainbow") {
+            t.style.backgroundColor = rainbow[Math.floor(Math.random() * rainbow.length)];
+        } 
     }
-    constructGrid(input);
 };
 
 function constructGrid(input) {
@@ -51,21 +57,18 @@ function constructGrid(input) {
     }
 };
 
+function getUserInput() {
+    input = prompt("How many rows and columns?");
+    if (input > 100) {
+        input = 100;
+    }
+    constructGrid(input);
+};
+
 function reconstructGrid() {
     container.replaceChildren();
     getUserInput();
-}
-
-function squareListener(event) {
-    let t = event.target;
-    if(t.className == "square") {
-        t.style.backgroundColor = palette;
-        if (palette == "rainbow") {
-            t.style.backgroundColor = rainbow[Math.floor(Math.random() * rainbow.length)];
-        } 
-    }
 };
 
-// getUserInput();
 constructGrid(40);
 
